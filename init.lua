58f9b40e0c1e59c1e822b53e8203a84aec8bdaaf
ambaradan <87865413+ambaradan@users.noreply.github.com>
2025-05-05 12:46:47 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -13,9 +13,9 @@ function M.setup(opts)
 end
 
 -- Keep existing require calls
-require("nvim-rockydocs.main")
-require("nvim-rockydocs.utils")
-require("nvim-rockydocs.mappings")
+-- require("nvim-rockydocs.main")
+-- require("nvim-rockydocs.utils")
+-- require("nvim-rockydocs.mappings")
 
 -- Expose the config for other modules
 M.config = config
