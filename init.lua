610824b513823c29c519ccda705f5dfaeff4d240
ambaradan <ambaradan@proton.me>
2025-05-05 22:47:14 +0200

    fix: simplify 'init.lua'

--- init.lua
+++ init.lua
@@ -1,6 +1,4 @@
--- lua/nvim-rockydocs/init.lua
-require("nvim-rockydocs.main")
-require("nvim-rockydocs.utils")
-require("nvim-rockydocs.mappings")
+-- nvim-rockydocs/init.lua
+require("nvim-rockydocs.init")
 
 return {}
