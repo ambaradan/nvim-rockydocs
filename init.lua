e041a7405a93fe42cee2b85251d8039a2758ca57
ambaradan <87865413+ambaradan@users.noreply.github.com>
2025-05-05 12:38:14 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,6 +1,23 @@
 -- lua/nvim-rockydocs/init.lua
+local M = {}
+
+-- Default configuration
+local config = {
+    port = "8000", -- Default port for mkdocs serve
+}
+
+-- Setup function
+function M.setup(opts)
+    opts = opts or {}
+    config.port = opts.port or config.port -- Update the port if provided
+end
+
+-- Keep existing require calls
 require("nvim-rockydocs.main")
 require("nvim-rockydocs.utils")
 require("nvim-rockydocs.mappings")
 
-return {}
+-- Expose the config for other modules
+M.config = config
+
+return M
